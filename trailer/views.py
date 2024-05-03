3c7b5fddc173a85c0f39fb8c3870b6ca4fa51863
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-05-03 19:12:28 +0100

    Added modify and delete cattle from trailer

--- trailer/views.py
+++ trailer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -24,3 +24,34 @@ def add_to_trailer(request, cattle_id):
     request.session['trailer'] = trailer
     print(request.session['trailer'])
     return redirect(redirect_url)
+
+
+def adjust_trailer(request, cattle_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+
+    trailer = request.session.get('trailer', {})
+
+    if quantity > 0:
+        trailer[cattle_id] = quantity
+    else:
+        trailer.pop(cattle_id)
+
+    request.session['trailer'] = trailer
+    return redirect(reverse('view_trailer'))
+
+
+def remove_from_trailer(request, cattle_id):
+    """Remove the item from the trailer"""
+
+    try:
+        trailer = request.session.get('trailer', {})
+
+        trailer.pop(cattle_id)
+
+        request.session['trailer'] = trailer
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)

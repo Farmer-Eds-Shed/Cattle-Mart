477ca71e5a095c4fde4ecf109d26666542e3a98d
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-05-06 20:28:40 +0100

    Change e-mail provider to gmail

--- cattle_mart/settings.py
+++ cattle_mart/settings.py
@@ -226,7 +226,7 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'cattle-mart@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
+    EMAIL_USE_TLS = False
     EMAIL_PORT = 25
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')

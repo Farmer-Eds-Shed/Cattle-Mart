acc4d161a01b4a1c31bf72fde6cbc86de367ca30
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-05-08 11:45:58 +0100

    Defined missing stripe charge variable in wh handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -24,10 +24,16 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        print("received")
         intent = event.data.object
         pid = intent.id
-        trailer = intent.metadata.trailer
+        trailer = intent.metadata.bag
         save_info = intent.metadata.save_info
+
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
 
         billing_details = stripe_charge.billing_details # updated
         shipping_details = intent.shipping

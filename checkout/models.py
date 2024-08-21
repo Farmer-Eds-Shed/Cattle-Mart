cdbcc7dc5008610a91f2d30095ee999724e33fc6
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-08-21 19:05:44 +0100

    Remove print statament with type error

--- checkout/models.py
+++ checkout/models.py
@@ -42,7 +42,6 @@ class Order(models.Model):
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         self.grand_total = self.order_total
-        print("does it update? " + self.order_total)
         self.save()
 
 

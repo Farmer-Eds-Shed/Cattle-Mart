0fef7d173828bb1be7373bc6c5d1b4380d412632
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-08-26 14:35:06 +0100

    Prevent webhook creating order for previously sold animal

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -126,6 +126,8 @@ class StripeWH_Handler:
                 )
                 for item_id, item_data in json.loads(trailer).items():
                     cattle = Cattle.objects.get(id=item_id)
+                    if cattle.sold:
+                        raise ValueError
                     if isinstance(item_data, int):
                         order_line_item = OrderLineItem(
                             order=order,
@@ -133,12 +135,23 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
+
+            except ValueError as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+            
             except Exception as e:
                 if order:
                     order.delete()
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+            
+           
+
         self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',

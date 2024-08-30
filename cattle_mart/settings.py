5a27ddba0cb1230ac29e9d1d390448483f79b860
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2024-08-30 18:20:14 +0100

    Set debug to False

--- cattle_mart/settings.py
+++ cattle_mart/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-1n!t)@93@85&#m$6i=@s(97lgxz!lx22=xy&f%!bsto8e$muga'
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'cattle-mart-42a123405ccb.herokuapp.com',
